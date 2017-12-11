1f3f2ee9ce25732db8314fbc77a3db9b38df0144
Krisjanis Balodis <krisjanis.balodis@gmail.com>
2017-12-11 09:12:30 +0200

    additional commit

--- LD3_Balodis.cpp
+++ LD3_Balodis.cpp
@@ -5,7 +5,7 @@
 #define ROWS 2
 #define COLS 2
 
-int main {
+int main() {
     int A[ROWS][COLS];
     int B[ROWS][COLS];
     int AB[ROWS][COLS];
@@ -16,8 +16,15 @@ int main {
         for (int j = 0; j < COLS; j++) {
             A[i][j] = rand();
             B[i][j] = rand();
-            C[i][j] = 1;
+            AB[i][j] = 0;
         }
+    }
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            std::cout<<A[i][j]<<" ";
+        }
+        std::cout<<std::endl;
     }
 
 
