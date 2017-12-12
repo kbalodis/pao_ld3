a30eb37e852f7594c731147f50bfba663057d4f7
krisjanis <krisjnais.balodis@playgineering.com>
2017-12-12 12:36:57 +0200

    implementation of matrix multiplication (initial)

--- LD3_Balodis.cpp
+++ LD3_Balodis.cpp
@@ -4,29 +4,37 @@
 
 #define ROWS 2
 #define COLS 2
+#define ROWCOLS 3
 
 int main() {
-    int A[ROWS][COLS];
-    int B[ROWS][COLS];
-    int AB[ROWS][COLS];
+    float A[ROWCOLS][ROWCOLS];
+    float B[ROWCOLS][ROWCOLS];
+    float AB[ROWCOLS][ROWCOLS];
 
     srand(time(NULL));
 
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
+    for (int i = 0; i < ROWCOLS; i++) {
+        for (int j = 0; j < ROWCOLS; j++) {
             A[i][j] = rand();
             B[i][j] = rand();
             AB[i][j] = 0;
         }
     }
 
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            std::cout<<A[i][j]<<" ";
+    for (int i = 0; i < ROWCOLS; i++) {
+        for (int j = 0; j < ROWCOLS; j++) {
+            for (int k = 0; k < ROWCOLS; k++) {
+                AB[i][j] = AB[i][j] + A[i][k] * B[k][j];
+            }
         }
-        std::cout<<std::endl;
     }
 
+    for (int i = 0; i < ROWCOLS; i++) {
+        for (int j = 0; j < ROWCOLS; j++) {
+            std::cout << AB[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
 }
