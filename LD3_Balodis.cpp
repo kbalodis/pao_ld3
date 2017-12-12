c735f6b303f8d9d3d85b145e68f5b2b2b3c07c6f
Krisjanis Balodis <krisjanis.balodis@gmail.com>
2017-12-12 18:28:39 +0200

    added advanced and transpose matrix multiples

--- LD3_Balodis.cpp
+++ LD3_Balodis.cpp
@@ -2,9 +2,55 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define ROWS 2
-#define COLS 2
-#define ROWCOLS 3
+#define ROWCOLS 500
+
+void basic_matrix_multiple(float A[ROWCOLS][ROWCOLS], float B[ROWCOLS][ROWCOLS], float AB[ROWCOLS][ROWCOLS]) {
+    for (int i = 0; i < ROWCOLS; i++) {
+        for (int j = 0; j < ROWCOLS; j++) {
+            for (int k = 0; k < ROWCOLS; k++) {
+                AB[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+// https://stackoverflow.com/questions/4300663/how-to-speed-up-matrix-multiplication-in-c
+void advanced_matrix_multiple(float A[ROWCOLS][ROWCOLS], float B[ROWCOLS][ROWCOLS], float AB[ROWCOLS][ROWCOLS]) {
+    for (int i = 0; i < ROWCOLS; i++) {
+        for (int k = 0; k < ROWCOLS; k++) {
+            for (int j = 0; j < ROWCOLS; j++) {
+                AB[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+template<typename T>
+void Transpose(int size, T** m)
+{
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            std::swap(m[i][j], m[j][i]);
+        }
+    }
+}
+
+// https://blogs.msdn.microsoft.com/xiangfan/2009/04/28/optimize-your-code-matrix-multiplication/
+void transpose_matrix_multiple(float A[ROWCOLS][ROWCOLS], float B[ROWCOLS][ROWCOLS], float AB[ROWCOLS][ROWCOLS])
+{
+    for (int i = 0; i < ROWCOLS; i++) {
+        for (int j = i + 1; j < ROWCOLS; j++) {
+            std::swap(B[i][j], B[j][i]);
+        }
+    }
+    for (int i = 0; i < ROWCOLS; i++) {
+        for (int j = 0; j < ROWCOLS; j++) {
+            for (int k = 0; k < ROWCOLS; k++) {
+                AB[i][j] += A[i][k] * B[j][k];
+            }
+        }
+    }
+}
 
 int main() {
     float A[ROWCOLS][ROWCOLS];
@@ -21,20 +67,9 @@ int main() {
         }
     }
 
-    for (int i = 0; i < ROWCOLS; i++) {
-        for (int j = 0; j < ROWCOLS; j++) {
-            for (int k = 0; k < ROWCOLS; k++) {
-                AB[i][j] = AB[i][j] + A[i][k] * B[k][j];
-            }
-        }
-    }
-
-    for (int i = 0; i < ROWCOLS; i++) {
-        for (int j = 0; j < ROWCOLS; j++) {
-            std::cout << AB[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
+    // basic_matrix_multiple(A, B, AB);
+    // advanced_matrix_multiple(A, B, AB);
+    transpose_matrix_multiple(A, B, AB);
 
     return 0;
 }
